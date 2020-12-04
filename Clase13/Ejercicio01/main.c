61cc16a79b355daaf221532bdbe71d60edae9264
gusgatam <gustavo.gatamora@gmail.com>
2020-12-04 16:32:58 -0300

    Devolución.
    Comentarios en los primeros ejercicios.

--- Clase13/Ejercicio01/main.c
+++ Clase13/Ejercicio01/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//Faltan funciones.
 int main()
 {
-    int resis[4], i, tot=0;
+    int resis[5], i, tot=0;// De 0 a 4 son 5 valores.
     printf("Ingresar 5 resistencias:\n");
     for(i=0; i<5; i++)
         scanf("%d",&resis[i]);
 
-    for(i=0; i<5; i++){
+    for(i=0; i<5; i++)
+    {
         if(i!=0)
             printf(" +");
         printf(" %d",resis[i]);
--- Clase13/Ejercicio02/main.c
+++ Clase13/Ejercicio02/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//Faltan funciones.
 int main()
 {
-    int num[4], i, mayor=0;
+    int num[5], i, mayor=0;//idem ejercicio anterior.
     float  prom=0;
     printf("Ingresar 5 valores: \n");
     for(i=0; i<5; i++)
         scanf("%d",&num[i]);
 
-    for(i=0; i<5; i++){
+    for(i=0; i<5; i++)
+    {
         prom += num[i];
         if (num[i]>mayor)
             mayor=num[i];
